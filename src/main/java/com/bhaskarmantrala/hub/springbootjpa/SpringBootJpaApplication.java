ad4f756e616a1b5d2b3182c89f12831a5177e0e1
BhaskarMantralaHub <97362616+BhaskarMantralaHub@users.noreply.github.com>
2022-04-20 12:44:25 -0700

    Added logger

--- src/main/java/com/bhaskarmantrala/hub/springbootjpa/SpringBootJpaApplication.java
+++ src/main/java/com/bhaskarmantrala/hub/springbootjpa/SpringBootJpaApplication.java
@@ -1,9 +1,11 @@
 package com.bhaskarmantrala.hub.springbootjpa;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class SpringBootJpaApplication {
 
     public static void main(String[] args) {
